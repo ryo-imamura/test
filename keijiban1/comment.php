a46871c7184098aba627bc316f1299c214c22817
ryo-imamura <www.ryo.i.co.jp@outlook.jp>
2020-07-16 20:22:14 +0900

    <?php  ?>を１つにまとめた

--- keijiban1/comment.php
+++ keijiban1/comment.php
@@ -1,10 +1,14 @@
-<?php require('model.php'); ?>
-<?php require('template.html'); ?>
+<?php require('model.php');
+      require('template.html');
 
 
-<?php
+
         if(isset($_POST['comment'])){
-          inputdb();
+          print('投稿しました');
+          //inputdb();
+          //コメントをデータベースに登録
+          $statement=$db->prepare('INSERT INTO comments SET comment=?, created_at=NOW()');
+          $statement->execute(array($_POST['comment']));
         }else{
 
         }
@@ -16,32 +20,38 @@
             $page=1;
         }
         $start=5*($page-1);
-          outputdb();
-        ?>
+        //outputdb();
+        //コメントをデータベースから取り出し
+        $comments=$db->prepare('SELECT * FROM comments ORDER BY id DESC LIMIT ?,5');
+        $comments->bindParam(1,$start,PDO::PARAM_INT);
+        $comments->execute();
 
-        <?php //コメント、時間を繰り返し表示 ?>
-        <?php while($comment=$comments->fetch()): ?>
-            <p>
-                <?php print($comment['comment']) ?>
-            </p>
-            <time><?php print($comment['created_at']); ?></time>
-            <hr>
-        <?php endwhile; ?>
+        //コメント、時間を繰り返し表示
+        while($comment=$comments->fetch()){
+            print('<p>');
+            print($comment['comment']);
+            print('</p>');
 
-        <?php
+            print('<time>');
+            print($comment['created_at']);
+            print('</time>');
+            print('<hr>');
+         }
+
+
         ////２ページ目以降なら前のページを表示する（１ページ目は表示しない）
-        if($page>=2):
-         ?>
-        <a href="comment.php?page=<?php print($page-1); ?>">前へ</a >
-        <?php endif; ?>
+        if($page>=2){
 
-        <?php
+        print('<a href="comment.php?page='.($page-1).'">前へ</a >');
+        }
+
         //最終ページは表示コメント数／5の繰り上げで表示
         $counts=$db->query('SELECT COUNT(*) AS cnt FROM comments');
         $count=$counts->fetch();
         $max_page=ceil($count['cnt']/5);
         //現在のページ＜最終ページなら次ページを表示する
-        if($page<$max_page):
-        ?>
-        <a href="comment.php?page=<?php print($page+1); ?>">次へ</a>
-        <?php endif; ?>
+        if($page<$max_page){
+        print('<a href="comment.php?page='.($page+1).'">次へ</a>');
+        }
+
+?>
