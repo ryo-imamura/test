1208f7fd8c1e2d5d7ebce4bd50c53e533a47ab9c
ryo-imamura <www.ryo.i.co.jp@outlook.jp>
2020-07-16 20:23:47 +0900

    関数の準備
    関数を呼び出すだけだとエラーになってしまうため、形だけ作成

--- keijiban1/model.php
+++ keijiban1/model.php
@@ -3,21 +3,16 @@
 try{
     $db=new PDO('mysql:dbname=keijiban1;host=127.0.0.1;charset=utf8','root','');
 
+    function inputdb(){
+      
+    }
+
+    function outputdb(){
+
+    }
 }catch(PDOException $e){
     echo 'DB接続エラー：'.$e->getMessage();
 }
 
-function inputdb(){
-  print('投稿しました');
-  //コメントをデータベースに登録
-  $statement=$db->prepare('INSERT INTO comments SET comment=?, created_at=NOW()');
-  $statement->execute(array($_POST['comment']));
-}
 
-function outputdb(){
-  //コメントをデータベースから取り出し
-  $comments=$db->prepare('SELECT * FROM comments ORDER BY id DESC LIMIT ?,5');
-  $comments->bindParam(1,$start,PDO::PARAM_INT);
-  $comments->execute();
-}
 ?>
