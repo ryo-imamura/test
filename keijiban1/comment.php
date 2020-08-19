95b70a46d8e59cc4044f1fabc5b0392ea2170887
ryo-imamura <www.ryo.i.co.jp@outlook.jp>
2020-08-19 10:22:49 +0900

    global変数不使用
    クラス、関数、引数、戻り値を使用してglobalを使わない仕様にした

--- keijiban1/comment.php
+++ keijiban1/comment.php
@@ -6,33 +6,19 @@
 
         if(isset($_POST['comment'])){
           print('投稿しました<br>');
-          inputdb();
-
+          $db=new Db;
+          $db->inputdb();
         }else{
 
         }
-
 
         if(isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])){
             $page=$_REQUEST['page'];
         }else{
             $page=1;
         }
-        $start=5*($page-1);
-        outputdb();
-
-        //コメント、時間を繰り返し表示
-        while($comment=$comments->fetch()){
-            print('<p>');
-            print($comment['comment']);
-            print('</p>');
-
-            print('<time>');
-            print($comment['created_at']);
-            print('</time>');
-            print('<hr>');
-         }
-
+        $db=new Db;
+        $db->outputdb(5*($page-1));
 
         ////２ページ目以降なら前のページを表示する（１ページ目は表示しない）
         if($page>=2){
@@ -41,9 +27,9 @@
         }
 
         //最終ページは表示コメント数／5の繰り上げで表示
-        countdb();
-        $count=$counts->fetch();
-        $max_page=ceil($count['cnt']/5);
+        $db=new Db;
+        $max_page=ceil($db->countdb()['cnt']/5);
+
         //現在のページ＜最終ページなら次ページを表示する
         if($page<$max_page){
         print('<a href="comment.php?page='.($page+1).'">次へ</a>');
--- keijiban1/model.php
+++ keijiban1/model.php
@@ -1,33 +1,47 @@
 <?php
+Class Db{
+  private static $statement='INSERT INTO comments SET comment=?, created_at=NOW()';
+  private static $comments='SELECT * FROM comments ORDER BY id DESC LIMIT ?,5';
+  private static $counts='SELECT COUNT(*) AS cnt FROM comments';
+  private static $pdo;
+  public function __construct(){
+    try{
+      self::$pdo = new PDO('mysql:dbname=keijiban1;host=127.0.0.1;charset=utf8','root','root');
 
-try{
-    $db=new PDO('mysql:dbname=keijiban1;host=127.0.0.1;charset=utf8','root','');
-
-
-}catch(PDOException $e){
-    echo 'DB接続エラー：'.$e->getMessage();
-}
-
-function inputdb(){
+    }catch(PDOException $e){
+      echo 'DB接続エラー：'.$e->getMessage();
+    }
+  }
   //コメントをデータベースに登録
-  global $db;
-  $statement=$db->prepare('INSERT INTO comments SET comment=?, created_at=NOW()');
-  $statement->execute(array($_POST['comment']));
+  public static function inputdb(){
+    $stmtI=self::$pdo->prepare(self::$statement);
+    $stmtI->execute(array($_POST['comment']));
+  }
+  //データベースからコメント取り出し
+  public static function outputdb($start){
+    $stmtO=self::$pdo->prepare(self::$comments);
+    $stmtO->bindParam(1,$start,PDO::PARAM_INT);
+    $stmtO->execute();
+    //コメント、時間を繰り返し表示
+    while($comment=$stmtO->fetch()){
+        print('<p>');
+        print($comment['comment']);
+        print('</p>');
+
+        print('<time>');
+        print($comment['created_at']);
+        print('</time>');
+        print('<hr>');
+     }
+  }
+  //コメントの数を把握
+  public static function countdb(){
+
+    $stmtC=self::$pdo->query(self::$counts);
+    $count=$stmtC->fetch();
+    return $count;
 }
 
-function outputdb(){
-  //コメントをデータベースから取り出し
-  global $comments;
-  global $db;
-  global $start;
-  $comments=$db->prepare('SELECT * FROM comments ORDER BY id DESC LIMIT ?,5');
-  $comments->bindParam(1,$start,PDO::PARAM_INT);
-  $comments->execute();
 }
 
-function countdb(){
-  global $db;
-  global $counts;
-  $counts=$db->query('SELECT COUNT(*) AS cnt FROM comments');
-}
 ?>
